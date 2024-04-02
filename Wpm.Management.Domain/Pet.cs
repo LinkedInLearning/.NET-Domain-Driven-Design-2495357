de920041ccc5e5394dfa61bdb6da136190d6cb4b
Rodrigo Díaz Concha <rdiazconcha@hotmail.com>
2024-04-02 00:14:28 -0600

    02_05f

--- Wpm.Management.Domain/Pet.cs
+++ Wpm.Management.Domain/Pet.cs
@@ -4,9 +4,17 @@ public class Pet : Entity
 {
     public string Name { get; init; }
     public int Age { get; init; }
-
+    public string Color { get; set; }
+    public decimal Weight { get; set; }
+    public SexOfPet SexOfPet { get; set; }
     public Pet(Guid id)
     {
         Id = id;
     }
+}
+
+public enum SexOfPet
+{
+    Male,
+    Female
 }

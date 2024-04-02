c24f30d06f90a1890e9ad87569e88a445248e823
Rodrigo Díaz Concha <rdiazconcha@hotmail.com>
2024-04-01 23:18:08 -0600

    01_01f

---
+++ Wpm.Management.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Wpm.Management.Domain.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ Wpm.Management.Domain/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace Wpm.Management.Domain;
+
+public class Class1
+{
+
+}

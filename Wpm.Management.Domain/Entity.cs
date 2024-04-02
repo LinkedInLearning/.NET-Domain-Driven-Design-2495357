0bdce25154eeea33e978ed9a9ddc1e21f6b67cfe
Rodrigo Díaz Concha <rdiazconcha@hotmail.com>
2024-04-01 23:47:47 -0600

    02_03f

---
+++ Wpm.Management.Domain/Entity.cs
@@ -0,0 +1,29 @@
+﻿namespace Wpm.Management.Domain;
+public abstract class Entity : IEquatable<Entity>
+{
+    public Guid Id { get; set; }
+
+    public bool Equals(Entity? other)
+    {
+        return other?.Id == Id;
+    }
+
+    public override bool Equals(object obj)
+    {
+        return Equals(obj as Pet);
+    }
+
+    public override int GetHashCode()
+    {
+        return Id.GetHashCode();
+    }
+    public static bool operator ==(Entity? left, Entity? right)
+    {
+        return left?.Id == right?.Id;
+    }
+
+    public static bool operator !=(Entity? left, Entity? right)
+    {
+        return left?.Id != right?.Id;
+    }
+}
--- Wpm.Management.Domain/Pet.cs
+++ Wpm.Management.Domain/Pet.cs
@@ -1,32 +1,7 @@
 ﻿namespace Wpm.Management.Domain;
 
-public class Pet : IEquatable<Pet>
+public class Pet : Entity
 {
-    public Guid Id { get; set; }
     public string Name { get; set; }
     public int Age { get; set; }
-
-    public bool Equals(Pet? other)
-    {
-        return other?.Id == Id;
-    }
-
-    public override bool Equals(object obj)
-    {
-        return Equals(obj as Pet);
-    }
-
-    public override int GetHashCode()
-    {
-        return Id.GetHashCode();
-    }
-    public static bool operator== (Pet? left, Pet? right)
-    {
-        return left?.Id == right?.Id;
-    }
-
-    public static bool operator !=(Pet? left, Pet? right)
-    {
-        return left?.Id != right?.Id;
-    }
 }

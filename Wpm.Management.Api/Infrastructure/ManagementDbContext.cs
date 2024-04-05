2dc5dd88a33171ddac51d2ec2b6a61bbf630fba7
Rodrigo Díaz Concha <rdiazconcha@hotmail.com>
2024-04-04 22:38:01 -0600

    04_01f

---
+++ Wpm.Management.Api/Infrastructure/ManagementDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Wpm.Management.Domain.Entities;
+
+namespace Wpm.Management.Api.Infrastructure;
+
+public class ManagementDbContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<Pet> Pets { get; set; }
+}
